e8e6d0299fb4d38b23279bd3e46f83bad5196f30
Harry-Whitehouse <harry.w1995@gmail.com>
2022-02-23 01:22:54 +0000

    arrow functions

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,22 +1,39 @@
 let question = {
-    title: "Gato",
-    alternative: ["Dog", "Cat", "Bird", "Fish"],
-    correctAnswer: 1,
+    title: "What is the name of a 30L bottle of wine",
+    alternative: ["Magnum", "Split", "Nabuchadnezzar", "Salmanazar"],
+    correctAnswer: 2,
 };
 
 let app = {
     start: function() {
         let alts = document.querySelectorAll(".alternative");
-
-        alts.forEach(function(element, index) {
-            element.addEventListener("click", function() {
-                console.log("Check correct answer");
+        alts.forEach((element, index) => {
+            element.addEventListener("click", () => {
+                this.checkAnswer(index);
             });
         });
+
+        //.bind allows to pass on the value of .this
+        // however, arrow functions do the same job and are much neater
+
+        // alts.forEach(
+        //     function(element, index) {
+        //         element.addEventListener(
+        //             "click",
+        //             function() {
+        //                 this.checkAnswer(index);
+        //             }.bind(this)
+        //         );
+        //     }.bind(this)
+        // );
 
         this.showQuestion(question);
     },
     showQuestion: function(q) {
+        //keep track of the current question
+
+        this.currentQuestion = q;
+
         let titleDiv = document.getElementById("title");
 
         titleDiv.textContent = q.title;
@@ -26,6 +43,14 @@ let app = {
         alts.forEach(function(element, index) {
             element.textContent = q.alternative[index];
         });
+    },
+
+    checkAnswer: function(userSelection) {
+        if (this.currentQuestion.correctAnswer == userSelection) {
+            alert("Correct!");
+        } else {
+            alert("Wrong!");
+        }
     },
 };
 
